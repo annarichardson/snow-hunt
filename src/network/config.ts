707a993c6c8075d51758a6f2a6b951bcfd38f13f
Anna Richardson <arichardson524@gmail.com>
2021-03-27 20:44:46 -0600

    setup .env file

--- src/network/config.ts
+++ src/network/config.ts
@@ -1,2 +1,2 @@
-export const API_ROOT = 'http://feeds.snocountry.net';
-export const API_KEY = 'SnoCountry.example'; // free development key
+export const API_ROOT = process.env.REACT_APP_API_ROOT;
+export const API_KEY = process.env.REACT_APP_API_KEY;
