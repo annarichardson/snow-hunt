e1efb128a8898aa3119092ddfe0c4c748339b5de
Anna Richardson <arichardson524@gmail.com>
2021-03-30 22:09:39 -0600

    added them util with colors

---
+++ src/utils/theme.ts
@@ -0,0 +1,15 @@
+export const Colors = {
+  transparent: 'rgba(0,0,0,0)',
+  white: '#fefdfd',
+
+  backgrounGrey: '#F4F6F8',
+  darkGrey: '#666666',
+
+  navyBlue: '#0a2b3a',
+  blue: '#405D89',
+  lightBlue: '#DAE6F9',
+};
+
+export type ColorKeys = keyof typeof Colors;
+
+export default Colors;
